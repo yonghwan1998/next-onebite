a59e150ff9c8de80a343034dd728c1cc2da8e08d
perfectoryinc <perfectoryinc@gmail.com>
2025-05-23 10:20:49 +0900

    style: 스타일링

--- section02/src/pages/search/index.tsx
+++ section02/src/pages/search/index.tsx
@@ -1,16 +1,16 @@
-import { useRouter } from "next/router";
 import { ReactNode } from "react";
 import SearchableLayout from "@/components/searchable-layout";
+import books from "@/mock/books.json";
+import BookItem from "@/components/book-item";
 
 export default function Page() {
 
-  const router = useRouter();
-
-  // const q = router.query.q;
-  const {q} = router.query;
-
   return (
-    <h1>SEARCH {q}</h1>
+    <div>
+      {books.map((book) => {
+        <BookItem key={book.id} {...book} />
+      })}
+    </div>
   );
 }
 
