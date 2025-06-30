f229ed5e1998169974dde5fb05eda6380561d04b
perfectoryinc <perfectoryinc@gmail.com>
2025-06-30 14:04:41 +0900

    feat: 리퀘스트 메모이제이션 학습
    - 서버 요청 전에 캐싱하여 중복으로 요청되는 요청에 대해 효율적으로 처리

--- section04/src/app/layout.tsx
+++ section04/src/app/layout.tsx
@@ -1,6 +1,29 @@
 import "./globals.css";
 import Link from "next/link";
 import style from "./layout.module.css";
+import { BookData } from "@/types";
+
+async function Footer() {
+
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`);
+
+  if(!response.ok) {
+    return (
+      <footer>제작 @yongss</footer>
+    );
+  }
+
+  const books: BookData[] = await response.json();
+  const bookCount = books.length;
+
+  return (
+    <footer>
+      <div>제작 @yongss</div>
+      <div>{bookCount}개의 도서가 등록되어 있습니다.</div>
+    </footer>
+  );
+
+}
 
 export default function RootLayout({
   children,
@@ -15,7 +38,7 @@ export default function RootLayout({
             <Link href={"/"}>📚 ONEBITE BOOKS</Link>
           </header>
           <main>{children}</main>
-          <footer>제작 @winterlood</footer>
+          <Footer />
         </div>
       </body>
     </html>
