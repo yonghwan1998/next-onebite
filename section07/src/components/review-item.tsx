09203899466751c1d706e2948c2d5de589e6a0e3
perfectoryinc <perfectoryinc@gmail.com>
2025-07-08 15:26:22 +0900

    feat: 리뷰 지우기 기능 추가

--- section07/src/components/review-item.tsx
+++ section07/src/components/review-item.tsx
@@ -1,3 +1,4 @@
+import ReviewItemDeleteButton from "./review-item-delete-button";
 import style from "./review-item.module.css";
 import { ReviewData } from "@/types";
 
@@ -13,8 +14,10 @@ export default function ReviewItem({
       <div className={style.author}>{author}</div>
       <div className={style.content}>{content}</div>
       <div className={style.bottom_container}>
-        <div className={style.date}>{new Date(createdAt).toLocaleDateString()}</div>
-        <div className={style.delete_btn}>삭제하기</div>
+        <div className={style.date}>{new Date(createdAt).toLocaleString()}</div>
+        <div className={style.delete_btn}>
+          <ReviewItemDeleteButton reviewId={id} bookId={bookId} />
+        </div>
       </div>
     </div>
   );
